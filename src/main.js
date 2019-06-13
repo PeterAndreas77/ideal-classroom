160be61a6aa370a94194352e70c9cde97369b8f9
PeterAndreas77 <andreas.soedharto@gmail.com>
2019-06-12 20:50:51 -0700

    added vue-router & vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import Amplify, { Auth } from 'aws-amplify'
+import awsconfig from './aws-exports'
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsconfig)
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(Vuetify)
 
 new Vue({
   render: h => h(App),
