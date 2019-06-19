0ce74ccef2db38cacc8862e08b2dab23e5f06720
PeterAndreas77 <andreas.soedharto@gmail.com>
2019-06-18 21:22:21 -0700

    added home components

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,44 @@
+<template src="./Home.html"></template>
+<style src="./Home.css"></style>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      slides: [
+        {
+          id: 1,
+          title: "Grades",
+          content: "Take control and keep track of your students' grades",
+          icon: "grade"
+        },
+        {
+          id: 2,
+          title: "Schedule",
+          content:
+            "Create and follow your own schedule or import an existing one",
+          icon: "schedule"
+        },
+        {
+          id: 3,
+          title: "Crafts",
+          content: "Search pinterest or other sites for your favorite crafts",
+          icon: "wallpaper"
+        }
+      ]
+    };
+  },
+  methods: {
+    next() {
+      const first = this.slides.shift();
+      this.slides = this.slides.concat(first);
+    },
+    previous() {
+      const last = this.slides.pop();
+      this.slides = [last].concat(this.slides);
+    }
+  }
+};
+</script>
+
